000dde4aeafbd18eef7652a580a2b45874396553
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-05-03 16:00:19 +0900

    feat: create problem `sort-numbers-abs`

---
+++ problems/sort-numbers-abs/sol-ac/main.cc
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  while (true) {
+    int n;
+    cin >> n;
+    if (n == 0) break;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end(), [](int a, int b) {
+      return abs(a) < abs(b);
+    });
+    for (int i = 0; i < n; i++) cout << v[i] << " \n"[i == n - 1];
+  }
+  return 0;
+}
