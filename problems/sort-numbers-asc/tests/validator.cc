aeeae00ac92651da94eb1b783ca07394552add71
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-05-03 15:34:40 +0900

    feat: create problem `sort-numbers-asc`

---
+++ problems/sort-numbers-asc/tests/validator.cc
@@ -0,0 +1,19 @@
+#include "testlib.h"
+
+int main(int argc, char* argv[]) {
+  registerValidation(argc, argv);
+  int testcases = 0;
+  while (true) {
+    int n = inf.readInt(0, 100, "n");  // 0 <= n <= 100
+    inf.readEoln();                    // 改行
+    if (n == 0) break;
+    testcases++;
+    for (int i = 0; i < n; i++) {
+      if (i > 0) inf.readSpace();               // 空白
+      inf.readInt(1, 100, format("a[%d]", i));  // 1 <= a[i] <= 100
+    }
+    inf.readEoln();  // 改行
+  }
+  ensuref(1 <= testcases && testcases <= 100, "testcases = %d", testcases);
+  inf.readEof();
+}
