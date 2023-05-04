8da74990463aa65a16860ae6829588f3045ad0b3
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-05-05 00:58:18 +0900

    feat: create problem `smallest-string-easy`

---
+++ problems/smallest-string-easy/sol-wa-printall/main.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> s(n);
+  for (int i = 0; i < n; i++) cin >> s[i];
+  sort(s.begin(), s.end());
+  for (int i = 0; i < n; i++) cout << s[i] << endl;
+  return 0;
+}
